e8aee114747fdbb46c4f8b66bf92284fe500ea31
Natchanon Charoensuk <inont@hotmail.co.th>
2018-04-01 13:44:33 +0700

    Add redux action for setState

--- src/redux.js
+++ src/redux.js
@@ -1,3 +1,4 @@
+const SET_STATE = "SET_STATE";
 const RESET_STATE = "RESET_STATE";
 
 const initialState = {
@@ -6,12 +7,22 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_STATE:
+      return {
+      ...state,
+      ...action.state
+    };
     case RESET_STATE:
       return initialState;
     default:
       return state;
   }
 }
+
+export const setState = (state) => ({
+  type: SET_STATE,
+  state
+});
 
 export const resetState = () => ({
   type: RESET_STATE
